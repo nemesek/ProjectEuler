4dc011304c7fd4b065ef215d110976e63715b4bf
Dan Nemesek <dnemesek@gmail.com>
2013-08-26 21:07:15 -0500

    Problem 19

---
+++ C#/Euler.Test/Problem19Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+
+namespace Euler.Test
+{
+    [TestClass]
+    public class Problem19Tests
+    {
+        [TestMethod]
+        public void Probem19_Can_CalcForSpecificYear()
+        {
+            // Arrange
+            var expected = 171;
+            var year = 1901;
+            var problem = new Problem19();
+
+            // Act
+            var result = problem.CalcForSpecificYear(year);
+
+            // Assert
+            Assert.AreEqual(expected, result);
+            
+        }
+    }
+}
---
+++ C#/ProjectEuler/Problem19.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEuler
+{
+    public class Problem19
+    {
+        static Dictionary<int, int> monthsDictionary = new Dictionary<int, int> 
+        {
+            {1,31}, {2, 28}, {3, 31}, {4,30}, {5, 31}, 
+            {6, 30}, {7, 31}, {8, 31}, {9, 30}, {10, 31},
+            {11, 30}, {12,31}
+        };
+        public int CalcForSpecificYear(int year)
+        {
+            var count = 0;
+
+            // First Sunday in 1901 falls on Jan 6th
+            var startDay = 6;
+
+            while (year < 2001)
+            {
+                for (var i = 1; i < 13; i++)
+                {
+                    if (startDay == 1)
+                        count++;
+
+                    var day = startDay;
+                    var daysInMonth = monthsDictionary[i];
+
+                    // Check for leap year
+                    if (i == 2 && year % 4 == 0)
+                    {
+                        daysInMonth = 29;
+                    }
+
+                    while (day <= daysInMonth)
+                    {
+                        day += 7;
+                    }
+
+                    // Grab next months starting day
+                    startDay = day - daysInMonth;
+                }
+
+                year++;
+            }
+
+            return count;
+        }
+    }
+}
