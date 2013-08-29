b7a63df1feaf0863b65618b9418383b61f7c5581
Dan Nemesek <dnemesek@gmail.com>
2013-08-28 19:48:03 -0500

    Problem 20

---
+++ C#/Euler.Test/Problem20Test.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+
+namespace Euler.Test
+{
+    [TestClass]
+    public class Problem20Test
+    {
+        [TestMethod]
+        public void Problem20_Can_SolveIt()
+        {
+            // Arrange
+            var solver = new Problem20();
+
+            // Act
+            var result = solver.SolveIt();
+
+            // Assert
+            //Assert.IsTrue(result == 3628800);
+            Assert.AreEqual(648, result);
+
+        }
+    }
+}
---
+++ C#/ProjectEuler/Problem20.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem20
+    {
+        public BigInteger SolveIt()
+        {
+            var num = Factorial(100);
+            var sum = FindSum(num);
+            return sum;
+        }
+
+        private BigInteger Factorial(int number)
+        {
+            if (number == 0) return 1;                    
+            return number * Factorial(number - 1);           
+                
+        }
+
+        private BigInteger FindSum(BigInteger num)
+        {
+            if (num < 1) return 0;
+            return num % 10 + FindSum(num / 10);
+        }
+    }
+}
