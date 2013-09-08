8c73f11a26d4837b18894b385ec6ab542ab375ca
Dan Nemesek <dnemesek@gmail.com>
2013-09-08 12:11:21 -0500

    Problem 3 in C# and Python

---
+++ C#/Euler.Test/Problem3Tests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+
+namespace Euler.Test
+{
+    [TestClass]
+    public class Problem3Tests
+    {
+        //The prime factors of 13195 are 5, 7, 13 and 29.
+        //What is the largest prime factor of the number 600851475143 ?
+
+        [TestMethod]
+        public void Problem3_Can_Solve()
+        {
+            // Arrange
+            var expected = 6857;
+            var target = new Problem3();
+
+            // Act
+            var result = target.Solve();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+
+        [TestMethod]
+        public void Problem3_IsPrime_ShouldReturnTrueFor29()
+        {
+            // Arrange
+            var primeNumber = 29;
+
+            // Act
+            var result = this.IsPrime(primeNumber);
+
+            // Assert
+            Assert.IsTrue(result);
+        }
+
+        private bool IsPrime(int primeNumber)
+        {
+            var squareRoot = (int)Math.Sqrt(primeNumber);
+            return !Enumerable.Range(2, squareRoot).Where(x => primeNumber % x == 0).Any();            
+        }
+
+    }
+}
---
+++ C#/ProjectEuler/Problem3.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    //The prime factors of 13195 are 5, 7, 13 and 29.
+    //What is the largest prime factor of the number 600851475143 ?
+
+    public class Problem3
+    {
+        public long Solve()
+        {
+            long max = 600851475143;
+
+            for (var i = 2; max > 0; i++)
+            {
+                if (max % i == 0)
+                {
+                    // Grab other factor since it will be larger and check to see if prime
+                    max /= i;
+                    if (IsPrime(max)) return max;
+                }
+            }
+
+            // If we are here then the original number must be prime...Thus it is the largest prime factor
+            return max;
+        }
+
+        private bool IsPrime(long candidate)
+        {
+            var squareRoot = (int)Math.Sqrt(candidate);
+            return !Enumerable.Range(2, squareRoot).Where(x => candidate % x == 0).Any();   
+
+        }
+    }
+}
