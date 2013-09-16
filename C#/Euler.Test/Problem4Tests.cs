6c1239b596d6bd40fc75ed3ca319400069a18d5e
Dan Nemesek <dnemesek@gmail.com>
2013-09-15 21:43:46 -0500

    Problem 4 in C#

---
+++ C#/Euler.Test/Problem4Tests.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+
+namespace Euler.Test
+{
+    [TestClass]
+    public class Problem4Tests
+    {
+        //A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
+        //Find the largest palindrome made from the product of two 3-digit numbers.
+
+        [TestMethod]
+        public void Problem4CanSolve()
+        {
+            // Arrange
+            var expected = 906609;
+            //var expected = 9009;
+            var target = new Problem4();
+
+            // Act
+            var result = target.Solve();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void Problem4_IsPalindromeTest()
+        {
+            // Arrange
+            var good = 906609;
+            var bad = 906509;
+
+            // Act
+            var goodResult = this.IsPalindrome(good);
+            var badResult = this.IsPalindrome(bad);
+
+            // Assert
+            Assert.IsTrue(goodResult);
+            Assert.IsFalse(badResult);
+
+        }
+
+        private bool IsPalindrome(int number)
+        {
+            var numDigits = Math.Floor(Math.Log10(number) + 1);
+            var half = numDigits / 2;
+            var count = 0;
+            double pre = 0;
+            double post = 0;
+
+            while (count < half)
+            {
+                var digit = number % 10;
+                number /= 10;
+                post += (Math.Pow(10, count) * digit);
+                count++;
+            }
+
+            if (numDigits % 2 == 1)
+            {
+                number /= 10;
+            }
+
+            count--;
+
+            while (count >= 0)
+            {
+                var digit = number % 10;
+                number /= 10;
+                pre += (Math.Pow(10, count) * digit);
+                count--;
+            }
+
+            if (pre == post) return true;
+
+            return false;
+        }
+    }
+}
---
+++ C#/ProjectEuler/Problem4.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public class Problem4
+    {
+        public int Solve()
+        {
+            return Enumerable.Range(100,899).SelectMany(x => Enumerable.Range(100,899), (x,y) => x*y).Where(p => this.IsPalindrome(p)).Max();
+        }
+
+        private bool IsPalindrome(int number)
+        {
+
+            var numDigits = Math.Floor(Math.Log10(number) + 1);
+            var half = numDigits / 2;
+            var count = 0;
+            double pre = 0;
+            double post = 0;
+
+            while (count < half)
+            {
+                var digit = number % 10;
+                number /= 10;
+                post += (Math.Pow(10, count) * digit);
+                count++;
+            }
+
+            if (numDigits % 2 == 1)
+            {
+                number /= 10;
+            }
+
+            count--;
+
+            while (count >= 0)
+            {
+                var digit = number % 10;
+                number /= 10;
+                pre += (Math.Pow(10, count) * digit);
+                count--;
+            }
+
+            if (pre == post) return true;
+
+            return false;
+        }
+    }
+}
