b70607091e23ecbe58cd5cba8bae358bea7fcf55
Dan Nemesek <dnemesek@gmail.com>
2013-09-16 20:40:23 -0500

    Refacor Py problem 4

--- Python/EulerPython/Euler/Problem4.py
+++ Python/EulerPython/Euler/Problem4.py
@@ -1,17 +1,21 @@
 #A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99.
 #Find the largest palindrome made from the product of two 3-digit numbers.
 
-def findMaxPalindrome(seq):
-    max = 0
-    for x,y in ((x,y) for x in seq for y in seq): 
-        if str(x*y) == str(x*y)[::-1]:
-            if x*y > max:
-                max = x*y
-    return max
+#def findMaxPalindrome(seq):
+#    max = 0
+#    for x,y in ((x,y) for x in seq for y in seq): 
+#        if str(x*y) == str(x*y)[::-1]:
+#            if x*y > max:
+#                max = x*y
+#    return max
 
+def findMaxPalindrome(seq):
+    return max([x*y for x in seq for y in seq if str(x*y) == str(x*y)[::-1]])
 
 seq = range(100,1000)
 print(findMaxPalindrome(seq))
+
+
 
 
 
