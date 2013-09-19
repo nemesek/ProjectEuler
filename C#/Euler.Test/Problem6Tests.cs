5f18edd96491ea7f8004d58b1d043d91f05bca3e
Dan Nemesek <dnemesek@gmail.com>
2013-09-18 20:31:05 -0500

    Problem 6 in Python and C#

---
+++ C#/Euler.Test/Problem6Tests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+
+namespace Euler.Test
+{
+    [TestClass]
+    public class Problem6Tests
+    {
+        [TestMethod]
+        public void Problem6_CanSolve()
+        {
+            // Arrange
+            var expected = 25164150;
+            var target = new Problem6();
+
+            // Act
+            var result = target.SolveIt();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+
+        }
+    }
+}
---
+++ C#/ProjectEuler/Problem6.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    //The sum of the squares of the first ten natural numbers is, 12 + 22 + ... + 102 = 385
+    //The square of the sum of the first ten natural numbers is, (1 + 2 + ... + 10)2 = 552 = 3025
+    //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
+    //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+    public class Problem6
+    {
+        public int SolveIt()
+        {
+            //var sumOfSquares = Enumerable.Range(1, 100).Select(x => x * x).Sum();
+            var sumOfSquares = Enumerable.Range(1, 100).Sum(x => x * x);
+            // var squareOfTheSum = (int) Math.Pow(Enumerable.Range(1, 100).Sum(), 2);
+            var squareOfTheSum = (100 * 101) * (100 * 101) / 4;
+            return squareOfTheSum - sumOfSquares;
+
+        }
+    }
+}
