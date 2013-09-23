3c483643cb9d011cae36f8bdc3f64b392b9ba3bc
Dan Nemesek <dnemesek@gmail.com>
2013-09-22 20:32:45 -0500

    Problem 8 in c#, Problem 1 in Haskell

---
+++ C#/Euler.Test/Problem8Tests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ProjectEuler;
+
+namespace Euler.Test
+{
+    [TestClass]
+    public class Problem8Tests
+    {
+        [TestMethod]
+        public void Problem8_Can_Solve()
+        {
+            // Arrange
+            var expected = 40824;
+            var target = new Problem8();
+
+            // Act
+            var result = target.SolveIt();
+
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
