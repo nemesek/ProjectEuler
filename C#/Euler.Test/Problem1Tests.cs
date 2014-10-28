dc16ccbe1c8697aa5c8c6eef1031af91b7042687
nemesek <dnemesek@gmail.com>
2014-10-28 16:35:49 -0500

    playing around with different C# styles

--- C#/Euler.Test/Problem1Tests.cs
+++ C#/Euler.Test/Problem1Tests.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ProjectEuler;
 
@@ -11,7 +9,7 @@ namespace Euler.Test
     [TestClass]
     public class Problem1Tests
     {
-
+        
         /*
          * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
          *  Find the sum of all the multiples of 3 or 5 below 1000.
@@ -21,14 +19,46 @@ namespace Euler.Test
         public void Problem1_Can_Solve()
         {
             // Arrange
-            var expected = 233168;
+            const int expected = 233168;
             var target = new Problem1();
-
+            
             // Act
             var result = target.Solve();
+            var iterativeResult = ImperativeSolution();
+            var functionalResult = FunctionalSolution();
+            
 
             // Assert
             Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, iterativeResult);
+            Assert.AreEqual(expected, functionalResult);
+        }
+
+        private static int ImperativeSolution()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var sum = 0;
+            for (var i = 1; i < 1000; i++)
+            {
+                if (i%3 == 0 || i%5 == 0)
+                {
+                    sum += i;
+                }
+            }
+            stopWatch.Stop();
+            Trace.WriteLine("Iterative Time elapsed: " + stopWatch.Elapsed);
+            return sum;
+        }
+
+        private static int FunctionalSolution()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            var result = Enumerable.Range(1, 999).Where(i => i%3 == 0 || i%5 == 0).Sum();
+            stopWatch.Stop();
+            Trace.WriteLine("Functional Time elapsed: " + stopWatch.Elapsed);
+            return result;
         }
     }
 }
--- C#/Euler.Test/Problem3Tests.cs
+++ C#/Euler.Test/Problem3Tests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,12 +21,16 @@ namespace Euler.Test
             // Arrange
             var expected = 6857;
             var target = new Problem3();
-
+            
             // Act
             var result = target.Solve();
+            var imperativeResult = ImperativeSolution();
+            var functionalResult = FunctionalSolution();
 
             // Assert
             Assert.AreEqual(expected, result);
+            Assert.AreEqual(expected, imperativeResult);
+            Assert.AreEqual(expected, functionalResult);
         }
 
 
@@ -36,16 +41,68 @@ namespace Euler.Test
             var primeNumber = 29;
 
             // Act
-            var result = this.IsPrime(primeNumber);
+            var result = IsPrime(primeNumber);
 
             // Assert
             Assert.IsTrue(result);
         }
 
-        private bool IsPrime(int primeNumber)
+        private static int ImperativeSolution()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            const long bigNum = 600851475143;
+            var max = (int) Math.Sqrt(bigNum);
+            for (var i = max; i > 2; i--)
+            {
+                if (bigNum%i != 0) continue;
+                if (!ImperativeIsPrime(i)) continue;
+                stopWatch.Stop();
+                Trace.WriteLine("Imperative: " + stopWatch.Elapsed);
+                return i;
+            }
+
+            stopWatch.Stop();
+            Trace.WriteLine("Imperative: " + stopWatch.Elapsed);
+            return 2;
+        }
+
+        private static int FunctionalSolution()
+        {
+            var stopWatch = new Stopwatch();
+            stopWatch.Start();
+            const long bigNum = 600851475143;
+            var max = (int)Math.Sqrt(bigNum);
+            //for (var i = max; i > 2; i--)
+            //{
+            //    if (bigNum % i != 0) continue;
+            //    if (IsPrime(i))
+            //    {
+            //        return i;
+            //    }
+            //}
+            var result = Enumerable.Range(2, max).Where(n => bigNum % n == 0 && IsPrime(n)).Max();
+            stopWatch.Stop();
+            Trace.WriteLine("Functional: " + stopWatch.Elapsed);
+            return result;
+        }
+
+        private static bool ImperativeIsPrime(int num)
+        {
+            var square = (int) Math.Sqrt(num);
+
+            for (var i = 2; i <= square; i++)
+            {
+                if (num % i == 0) return false;
+            }
+
+            return true;
+        }
+
+        private static bool IsPrime(int primeNumber)
         {
             var squareRoot = (int)Math.Sqrt(primeNumber);
-            return !Enumerable.Range(2, squareRoot).Where(x => primeNumber % x == 0).Any();            
+            return Enumerable.Range(2, squareRoot).All(x => primeNumber % x != 0);            
         }
 
     }
