4b42e7de140b93e3b563c47aa2ac032213bd3733
Nemesek <dnemesek@gmail.com>
2015-01-23 11:45:43 -0600

    adding comments to problem 3

--- Haskell/Problem3.hs
+++ Haskell/Problem3.hs
@@ -1,6 +1,6 @@
 primeFactors :: [Integer] -> Integer -> Integer -> [Integer]
 primeFactors acc n x
-  | n `mod` x == 0 = primeFactors(x:acc)(n `div` x) x
+  | n `mod` x == 0 = primeFactors(x:acc)(n `div` x) x -- This builds up the list of prime factors
   | n <= 2 = acc
   | otherwise = primeFactors acc n (x+1)
 solve :: Integer
