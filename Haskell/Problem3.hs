05acc336d8fa63438ba19d72d6f262b1a9de80f0
Nemesek <dnemesek@gmail.com>
2015-01-23 11:13:40 -0600

    problem 3 in haskell

---
+++ Haskell/Problem3.hs
@@ -0,0 +1,8 @@
+primeFactors :: [Integer] -> Integer -> Integer -> [Integer]
+primeFactors acc n x
+  | n `mod` x == 0 = primeFactors(x:acc)(n `div` x) x
+  | n <= 2 = acc
+  | otherwise = primeFactors acc n (x+1)
+solve :: Integer
+solve = foldr max 0 $ primeFactors [] 600851475143 3
+main = print solve
