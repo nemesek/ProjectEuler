0c9d5e7eadfb4a426719e21553bf1ecb759a12ee
Nemesek <dnemesek@gmail.com>
2015-01-24 01:57:02 -0600

    cleaning up problem 3

--- Haskell/Problem3.hs
+++ Haskell/Problem3.hs
@@ -1,8 +1,9 @@
-primeFactors :: [Integer] -> Integer -> Integer -> [Integer]
-primeFactors acc n x
-  | n `mod` x == 0 = primeFactors(x:acc)(n `div` x) x -- This builds up the list of prime factors
-  | n <= 2 = acc
-  | otherwise = primeFactors acc n (x+1)
+filterPrimes :: [Integer] -> Integer -> Integer -> [Integer]
+filterPrimes acc n x
+  | n `mod` x == 0 = filterPrimes(x:acc)(n `div` x) x -- This builds up the list of prime factors, and reduces n
+  | n <= 2 = acc  -- base case
+  | otherwise = filterPrimes acc n (x+1) -- ow, increment x
+
 solve :: Integer
-solve = foldr max 0 $ primeFactors [] 600851475143 3
+solve = foldr max 0  (filterPrimes [] 600851475143 3)
 main = print solve
