2d81e99a99e06fd7945e3266b2bf565684a7ef4a
Nemesek <dnemesek@gmail.com>
2015-01-29 17:44:59 -0600

    my solution for problem 4

--- Haskell/Problem4.hs
+++ Haskell/Problem4.hs
@@ -1,1 +1,6 @@
-solve = maximum [x | y <-[100..999], z <- [y..999], let x = y*z, let s = show x, s == reverse s]
+reverseInt :: Integer -> Integer
+reverseInt = read . reverse . show
+solve = maximum [if p == reverseInt p then p else 0 | x <-[100..999], y <- [x..999], let p = x * y]
+-- this is a test
+solve' = maximum [x | y <-[100..999], z <- [y..999], let x = y*z, let s = show x, s == reverse s]
+
