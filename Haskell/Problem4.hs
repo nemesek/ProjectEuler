4f2c0a3b392b2b8c915d2aca2d4cab37efb39b22
Nemesek <dnemesek@gmail.com>
2015-01-29 21:56:46 -0600

    another soln

--- Haskell/Problem4.hs
+++ Haskell/Problem4.hs
@@ -1,23 +1,25 @@
 reverseInt :: Integer -> Integer
 reverseInt = read . reverse . show
 
-solve = maximum [if p == reverseInt p then p else 0 | x <-[100..999], y <- [x..999], let p = x * y]
-
-
--- this is soln I got from the Hasekll wiki -- seems to be a lot faster than mine
-solve' = maximum [x | y <-[100..999], z <- [y..999], let x = y*z, let s = show x, s == reverse s]
-
-
-solve2 = maximum [p | x <- [100..999], y <- [x..999], let p = x * y, p == reverseInt p]
-
-{-reverseInt' :: Integer -> Integer
-reverseInt' n = aux n 0
-	where aux 0 y = y
-		  aux x y = let (x', y') = x `quotRem` 10
-		  	in aux x' (10*y+y') 
-
-solve3 = maximum[p | x <- [100..999], y <- [x..999], let p = x * y, p == reverseInt' p]-}
 reverseIntToString :: Integer -> String 
 reverseIntToString = reverse . show
 
-solve3 = maximum [p | x <- [100..999], y <- [x..999], let p = x * y, show p == reverseIntToString p]
+reverseInt' :: Integer -> Integer
+reverseInt' 0 = 0
+reverseInt' n = mod n 10 * 10^place + reverseInt' (div n 10)
+	where
+		n' = fromIntegral n
+		place = (floor . logBase 10) n'
+
+solve = maximum [if p == reverseInt p then p else 0 | x <-[100..999], y <- [x..999], let p = x * y] -- slow
+
+-- this is soln I got from the Hasekll wiki -- seems to be a lot faster than mine
+solve' = maximum [x | y <-[100..999], z <- [y..999], let x = y*z, let s = show x, s == reverse s] -- fast
+
+solve2 = maximum [p | x <- [100..999], y <- [x..999], let p = x * y, p == reverseInt p] -- slow
+
+solve3 = maximum [p | x <- [100..999], y <- [x..999], let p = x * y, show p == reverseIntToString p] -- fast
+
+solve4 = maximum [p | x <- [100..999], y <- [x..999], let p = x * y, show p == reverse (show p)]	-- fast
+
+solve5 = maximum [p | x <- [100..999], y <- [x..999], let p = x * y, p == reverseInt' p] -- slow
