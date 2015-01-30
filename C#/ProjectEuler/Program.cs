cecd2a07f4a55530460d849df44cb62d5c40165b
nemesek <dnemesek@gmail.com>
2015-01-30 16:32:14 -0600

    Redid problem 5

--- C#/ProjectEuler/Program.cs
+++ C#/ProjectEuler/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace ProjectEuler
 {
@@ -9,23 +6,27 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            var start = DateTime.Now;
-            var p = new Problem4();
-            for(var i = 0; i < 5; i++)
-            {
-                var sw = System.Diagnostics.Stopwatch.StartNew();
-                p.Solve();
-                sw.Stop();
-                Console.WriteLine("Linq Time taken: " + sw.ElapsedMilliseconds);
+            var p = new Problem5();
+            var sw = System.Diagnostics.Stopwatch.StartNew();
+            p.Solve();
+            sw.Stop();
+            Console.WriteLine("Time taken " + sw.ElapsedMilliseconds);
+
+            //for(var i = 0; i < 5; i++)
+            //{
+            //    var sw = System.Diagnostics.Stopwatch.StartNew();
+            //    p.Solve();
+            //    sw.Stop();
+            //    Console.WriteLine("Linq Time taken: " + sw.ElapsedMilliseconds);
 
 
-                var sw2 = System.Diagnostics.Stopwatch.StartNew();
-                p.SolveImperative();
-                sw2.Stop();
-                Console.WriteLine("Imperative Time taken: " + sw2.ElapsedMilliseconds);
-                Console.WriteLine("=============================================");
-            }        
-                 
+            //    var sw2 = System.Diagnostics.Stopwatch.StartNew();
+            //    p.SolveImperative();
+            //    sw2.Stop();
+            //    Console.WriteLine("Imperative Time taken: " + sw2.ElapsedMilliseconds);
+            //    Console.WriteLine("=============================================");
+            //}        
+
         }
     }
 }
