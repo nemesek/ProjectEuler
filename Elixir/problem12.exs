05ada3e74c289c10a990c0b75fb962d7a9aef496
Nemesek <dnemesek@gmail.com>
2015-03-06 22:44:39 -0600

    some elixir optimizations

--- Elixir/problem12.exs
+++ Elixir/problem12.exs
@@ -3,36 +3,18 @@ defmodule Problem12 do
   defp _solve(_, candidate, _, true) do candidate end
   defp _solve(n, _, numDiv, false) do
     candidate = getTriangularFast(n)
-    # d1+d2+(d1-1)*(d2-1)-1 where d1,d2 are the divisor count of the factors (i, i+1/2)
-    #d1 = getNumDivisors(n)
-    #d2 = getNumDivisors(div((n+1),2))
-    #numDivisors = d1 + d2 + (d1 - 1) * (d2 - 1) - 1
-    #isLarger = numDivisors > numDiv
-    _solve(n+1, candidate, numDiv, getNumFactors(candidate) > numDiv)
-    #_solve(n+1, candidate, numDiv, isLarger)
+    _solve(n+1, candidate, numDiv, getNumDivisors(candidate) > numDiv)
   end
 
-  # def getNumDivisors(n) do _getNumDivisors(n,n,0) end
-  # defp _getNumDivisors(_, 1, acc) do 1 + acc end
-  # defp _getNumDivisors(n, d, acc) do
-  #   cond do
-  #     n == d -> _getNumDivisors(n, div(d,2), acc + 1)
-  #     rem(n,d) == 0 -> _getNumDivisors(n, d-1, acc + 1)
-  #     true -> _getNumDivisors(n, d-1, acc)
-  #   end
-  # end
-
-  defp getNumFactors(n) do length(_factors(n, div(n, 2))) + 1 end
-  defp _factors(1, _), do: [1]
-  defp _factors(_, 1), do: [1]
-  defp _factors(n, i) do
-    if rem(n, i) == 0 do
-      [i| _factors(n, i-1)]
-    else
-      _factors(n, i-1)
+  def getNumDivisors(n) do _getNumDivisors(n,round(:math.sqrt(n)),0) end
+  defp _getNumDivisors(_, 1, acc) do 2 + acc end
+  defp _getNumDivisors(n, d, acc) do
+    cond do
+      # n == d -> _getNumDivisors(n, div(d,2), acc + 1)
+      rem(n,d) == 0 -> _getNumDivisors(n, d - 1, acc + 2)
+      true -> _getNumDivisors(n, d - 1, acc)
     end
   end
-
 
   def getTriangularFast(n) do div(n * (n+1),2) end
   defp getTriangular(n) do _getTriangular(n, 0) end
@@ -44,7 +26,7 @@ end
 
 defmodule PrimeFactors do
   def of(n) do
-    factors(n, div(n, 2)) |> Enum.filter(&is_prime?/1)
+    factors(n, div(n, 2)) #|> Enum.filter(&is_prime?/1)
   end
 
   def factors(1, _), do: [1]
@@ -59,6 +41,44 @@ defmodule PrimeFactors do
 
   def is_prime?(n) do
     factors(n, div(n, 2)) == [1]
+  end
+end
+
+
+
+defmodule Sieve do
+  def eratosthenes(limit) do
+    _eratosthenese(limit, :math.sqrt(limit), 2, HashSet.new)
+  end
+
+  defp _eratosthenese(limit, sqrtLimit, n, notPrimeSet) when n > sqrtLimit do
+    addNumbersNotInHashSetToList(notPrimeSet, limit)
+  end
+
+  defp _eratosthenese(limit, sqrtLimit, n, notPrimeSet) do
+    nPlus1 = n + 1
+
+    if isInNotPrimes(n, notPrimeSet) do
+      _eratosthenese(limit, sqrtLimit, nPlus1, notPrimeSet)
+    else
+      newNotPrimeHash = sieveMultipleOf(n, limit, notPrimeSet)
+      _eratosthenese(limit, sqrtLimit, nPlus1, newNotPrimeHash)
+    end
+  end
+
+  defp sieveMultipleOf(n, limit, notPrimeSet) do
+    _sieveMultipleOf(n, limit, notPrimeSet, n + n)
+  end
+
+  defp _sieveMultipleOf(_, limit, notPrimeSet, notPrime) when notPrime > limit, do: notPrimeSet
+  defp _sieveMultipleOf(n, limit, notPrimeSet, notPrime) do
+    _sieveMultipleOf(n, limit, notPrimeSet |> Set.put(notPrime), notPrime + n)
+  end
+
+  defp isInNotPrimes(n, notPrimeSet), do: Enum.member?(notPrimeSet, n)
+
+  defp addNumbersNotInHashSetToList(notPrimeSet, limit) do
+    for n <- 2..limit, not isInNotPrimes(n, notPrimeSet), do: n
   end
 end
 
