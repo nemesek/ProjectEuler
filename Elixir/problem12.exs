09628fe8003ed46d435241bd2dbce4dee5d67098
Nemesek <dnemesek@gmail.com>
2015-03-08 19:53:25 -0500

    redid problem 12 with tau

--- Elixir/problem12.exs
+++ Elixir/problem12.exs
@@ -15,6 +15,34 @@ defmodule Problem12 do
       _getNumDivisors(n, d - 1, acc)
     end
   end
+
+  # faster
+  def solve2(numDiv) do _solve2(1, numDiv, false, 1) end
+  defp _solve2(_, _, true, acc) do acc end
+  defp _solve2(n, numDiv, false, acc) do
+    acc = acc + n + 1
+    _solve2(n+1, numDiv, getNumFactors(acc) > numDiv, acc)
+  end
+
+  # my implementation of tau to get num factors
+  def getNumFactors(n) do
+    primeFactors(n)
+    |> Enum.group_by(fn x -> x end)
+    |> Dict.values
+    |> Enum.map(fn x -> length(x) + 1 end)
+    |> List.foldr(1, fn (x,y) -> x * y end)
+  end
+
+  defp primeFactors(n) do _primeFactors(n,2,[]) end
+  defp _primeFactors(1, _, acc) do acc end
+  defp _primeFactors(2, _, acc) do [2|acc] end
+  defp _primeFactors(n, d, acc) do
+    if(rem(n,d) == 0) do
+      _primeFactors(div(n,d), d, [d|acc])
+    else
+      _primeFactors(n, d + 1, acc)
+    end
+  end
 end
 #:timer.tc(Problem12,:solve,[500])
 
